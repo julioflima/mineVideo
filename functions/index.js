9781da3deeccbfbbd98a83c650e1c166c871e8c4
juloko <julio_flima@hotmail.com>
2019-03-04 21:22:25 -0300

    template done without map

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const request = require('request');
+const cheerio = require('cheerio');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+admin.firestore().settings({ timestampsInSnapshots: true });
+
+exports.submit = require('./textRobot/somethingToWatch');
+
--- public/js/mineVideo.js
+++ public/js/mineVideo.js
@@ -1,22 +1,30 @@
 const mineNow = document.getElementById('mineNow');
 const somethingToRise = document.getElementById('somethingToRise');
+const consoleRemote = document.getElementById('console');
 
 if (somethingToRise) {
     somethingToRise.addEventListener("keyup", function (e) {
-      e.preventDefault();
-      if (e.keyCode == 13 && somethingToRise.value !== "") {
-        cloudComputing(somethingToRise.value);
-      }
+        e.preventDefault();
+        if (e.keyCode == 13 && somethingToRise.value !== "") {
+            cloudComputing(somethingToRise.value);
+        }
     });
-  }
+}
 
-  if (mineNow) {
+if (mineNow) {
     mineNow.addEventListener("click", function (e) {
         cloudComputing(somethingToRise.value);
     });
-  }
+}
 
-  const cloudComputing = function(somethingSearch){
+const cloudComputing = function (somethingSearch) {
     alert(somethingSearch);
 
-  };
+};
+
+setInterval(function () {
+    if (consoleRemote.scrollTop < consoleRemote.scrollHeight) {
+        consoleRemote.scrollTop += 1;
+    }
+}, 10)
+
